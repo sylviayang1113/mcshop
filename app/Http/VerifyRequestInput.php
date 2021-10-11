31dabe2a6b261273d0a5396b1d43b3fbf8aebe07
interesting1113 <sylviayang1113@gmail.com>
2021-10-11 19:09:50 +0900

    verify rules

--- app/Http/VerifyRequestInput.php
+++ app/Http/VerifyRequestInput.php
@@ -4,7 +4,40 @@
 namespace App\Http;
 
 
+use App\Exceptions\BusinessException;
+
 trait VerifyRequestInput
 {
+    /**
+     * @param $key
+     * @param null $default
+     * @return mixed
+     * @throws BusinessException
+     */
+    public function  verifyString($key, $default = null)
+    {
+        return $this->verifyData($key, $default, 'string');
+    }
 
+    /**
+     * @param $key
+     * @param null $default
+     * @return mixed
+     * @throws BusinessException
+     */
+    public function verifyBoolean($key, $default = null)
+    {
+        return $this->verifyData($key, $default, 'boolean');
+    }
+
+    /**
+     * @param $key
+     * @param null $default
+     * @return mixed
+     * @throws BusinessException
+     */
+    public function verifyPositiveInteger($key, $default = null)
+    {
+        return $this->verifyData($key, $default, 'integer|min:1');
+    }
 }
