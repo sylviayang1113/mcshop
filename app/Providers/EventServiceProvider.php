7303cebbd75abd801112f1583bb95c673e33a946
interesting1113 <sylviayang1113@gmail.com>
2021-11-02 23:04:33 +0900

    输出sql日志

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
@@ -27,6 +29,25 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        parent::boot();
+
+        DB::listen(function (QueryExecuted $event) {
+            $sql = $event->sql;
+            $bindings = $event->bindings;
+            $time = $event->time;
+
+            $bindings = array_map(function ($binding) {
+                if (is_string($binding)) {
+                    return "'$binding'";
+                } else if ($binding instanceof \DateTime) {
+                    return $binding->format("'Y-m-d H:i:s'");
+                }
+                return $binding;
+            }, $bindings);
+
+            $sql = str_replace('?', '%s', $sql);
+            $sql = sprintf($sql, ...$bindings);
+            // $sql = vsprintf($sql, $bindings);
+        });
     }
 }
