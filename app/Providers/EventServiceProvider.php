52897ded46486ce4f7946e1972a05a88f3f1e0d4
interesting1113 <sylviayang1113@gmail.com>
2021-11-03 15:56:32 +0900

    添加监听

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace App\Providers;
 
+use App\Listeners\DBSqlListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,9 +19,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+        QueryExecuted::class => [
+            DBSqlListener::class
+        ]
     ];
 
     /**
@@ -31,23 +33,5 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        DB::listen(function (QueryExecuted $event) {
-            $sql = $event->sql;
-            $bindings = $event->bindings;
-            $time = $event->time;
-
-            $bindings = array_map(function ($binding) {
-                if (is_string($binding)) {
-                    return "'$binding'";
-                } else if ($binding instanceof \DateTime) {
-                    return $binding->format("'Y-m-d H:i:s'");
-                }
-                return $binding;
-            }, $bindings);
-
-            $sql = str_replace('?', '%s', $sql);
-            $sql = sprintf($sql, ...$bindings);
-            // $sql = vsprintf($sql, $bindings);
-        });
     }
 }
